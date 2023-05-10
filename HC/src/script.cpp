2c0ecd94f51b2aa79012680bc348aa8329a4941c
Salah Eldin <samhgrrm@gmail.com>
2023-05-11 01:15:53 +0300

    medhat v0.3

--- HC/src/script.cpp
+++ HC/src/script.cpp
@@ -4,6 +4,8 @@ using namespace std;
 typedef long long ll;
 
 void solve() {
+  // todo: add info about the algorithm.
+
   // Read the message to be encoded
   string mgs;
   cin >> mgs;
@@ -17,11 +19,9 @@ void solve() {
   ll n = k + r;
 
   // Initialize the code word with all zeros
-  string codeWord;
-  codeWord.assign(n, '0');
+  string codeWord(n, '0');
 
   int a = 0;
-
   // Fill the data bits in the code word
   for (int i = n - 1, j = 0; i >= 0; i--, j++) {
     // Skip the positions that are powers of 2 (parity bits)
@@ -35,13 +35,13 @@ void solve() {
   int x = 1;
   reverse(codeWord.begin(), codeWord.end());
   for (int i = 0; i < r; i++) {
-    res_of_Ps[i] = 0;
+    // res_of_Ps[i] = 0;
     int cnt = (1 << i) - 1;
     bool check = true;
     for (int k = cnt; k < n; k += x) {
       if (check) {
         for (int t = k; t < k + x && t < n; t++) {
-          res_of_Ps[i] = (res_of_Ps[i] ^ codeWord[t] - '0');
+          res_of_Ps[i] ^= (codeWord[t] - '0');
         }
       }
       check = !check;
@@ -62,38 +62,42 @@ void solve() {
   cout << "\n";
   cout << "*******************\n";
 
-  // Read the received code word
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  //   Read the received code word
   string recivedCodeWord;
   cin >> recivedCodeWord;
   vector<long long> res_of_Ps_InResivedCodeWord(r);
+  int error = 0;
   x = 1;
   reverse(recivedCodeWord.begin(), recivedCodeWord.end());
-
   // Calculate the values of parity bits in the received code word
   for (int i = 0; i < r; i++) {
-    res_of_Ps_InResivedCodeWord[i] = 0;
+    // res_of_Ps_InResivedCodeWord[i] = 0;
     int cnt = (1 << i) - 1;
-    bool check = true;
+    bool check = 1;
     for (int k = cnt; k < n; k += x) {
       if (check) {
         for (int t = k; t < k + x && t < n; t++) {
           if (k == cnt && k == t) continue;
-          res_of_Ps_InResivedCodeWord[i] = (res_of_Ps_InResivedCodeWord[i] ^ recivedCodeWord[t] - '0');
+          res_of_Ps_InResivedCodeWord[i] ^= (recivedCodeWord[t] - '0');
         }
       }
       check = !check;
     }
+
     x *= 2;
   }
-  reverse(recivedCodeWord.begin(), recivedCodeWord.end());
 
-  // Compare the calculated parity bits with the received parity bits to detect errors
-  int error = 0;
-  for (ll i = 0; i < r; i++) {
-    if (res_of_Ps[i] != res_of_Ps_InResivedCodeWord[i]) {
-      error += i + 1;
+  for (int i = 0; i < r; i++) {
+    int a = (1 << i);
+
+    if (res_of_Ps_InResivedCodeWord[i] != (recivedCodeWord[a - 1] - '0')) {
+      error += a;
     }
   }
+
+  reverse(recivedCodeWord.begin(), recivedCodeWord.end());
 
   // If no error is detected and the received code word matches the original code word
   if (error == 0 && recivedCodeWord == codeWord) {
@@ -102,28 +106,16 @@ void solve() {
     cout << "Code word: " << recivedCodeWord;
     cout << "\n";
   } else {
-    // If no error is detected but the received code word does not match the original code word
-    if (error == 0) {
-      // Find the position of the bit that is different
-      for (ll i = 0; i < n; i++) {
-        if (codeWord[i] != recivedCodeWord[i]) {
-          error = n - i;
-          break;
-        }
-      }
-    }
-
     cout << "Error in bit number " << error;
     cout << "\n";
 
     // Flip the bit at the detected error position to correct the error
-    if (recivedCodeWord[n - error] == '0') {
+    if (recivedCodeWord[n - error] == '0')
       recivedCodeWord[n - error] = '1';
-    } else {
+    else
       recivedCodeWord[n - error] = '0';
-    }
 
-    cout << "Correct code word: " << codeWord;
+    cout << "Correct code word: " << recivedCodeWord;
     cout << "\n";
   }
 }
